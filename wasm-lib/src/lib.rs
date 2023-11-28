aca2406080d8a03b0eeee77be1c555cbc05584d5
abhi3700 <dalvath3700@gmail.com>
2023-11-29 01:23:19 +0530

    Added the blog url

--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -1,3 +1,6 @@
+//! This lib is to add a few functions to call from React TS.
+//! Source: https://www.tkat0.dev/posts/how-to-create-a-react-app-with-rust-and-wasm/
+
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
